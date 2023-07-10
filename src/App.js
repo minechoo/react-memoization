08e4fedabaabac8051ba5ea8be6b0404d6c46ec0
minechoo <choominhae@gmail.com>
2023-07-10 09:59:55 +0900

    prop 전달되는 경우

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Child from './Child';
 function App() {
 	console.log('parent');
 	const [Counter, setCounter] = useState(0);
+	const [Input, setInput] = useState('');
+
 	return (
 		<div>
 			<h1>Parent</h1>
 			<button onClick={() => setCounter(Counter + 1)}>plus</button>
 			<p>{Counter}</p>
-			<Child />
+			<input type='text' value={Input} onChange={(e) => setInput(e.target.value)} />
+			<Child Counter={Counter} />
 		</div>
 	);
 }
